9c0a5d998b424f007106191286f1e972da60dda2
nrbrt <norklaus@gmail.com>
2021-09-04 05:21:53 +0200

    Update install.sh
    changed back to original avrgirl-arduino, instead of @dawee fork, because it no longer works with new versions of node.js

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir /home/pi/mega;
 cd /home/pi/mega;
-npm install @dawee/avrgirl-arduino;
+npm install avrgirl-arduino;
 wget -O mega2560-dual-smt172-needlevalve.ino.mega.hex https://raw.githubusercontent.com/nrbrt/2560pro-smt172-needlevalve/master/mega2560-dual-smt172-needlevalve.ino.mega.hex;
 wget -O programmega.js https://raw.githubusercontent.com/nrbrt/2560pro-smt172-needlevalve/master/programmega.js
 node /home/pi/mega/programmega.js;
